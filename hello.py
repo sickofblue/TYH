a62c27b68ffc511d6a4c81950ee0ab88623888c7
sickofblue <1586466296@qq.com>
2021-07-31 13:48:26 +0800

    templates

--- hello.py
+++ hello.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1>hello world!</h1>'
+    return render_template('index.html')
 
 @app.route('/user/<name>')
 def usere(name):
-    return '<h1>hello,{}!</h1>'.format(name)
+    return render_template('user.html',name=name)
